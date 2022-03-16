8e18ea38dad8628d0c22d98bd3edb0f26bd751c3
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-16 18:44:34 -0300

    primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,22 +1,8 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...id) =>
-  // eslint-disable-next-line array-callback-return
-  data.species.filter((animal) => {
-    const bichos = [];
-    if (animal.id === id[0] || animal.id === id[1]) {
-      bichos.push(animal);
-      return bichos;
-    }
-  });
-
-// console.log(
-//   getSpeciesByIds(
-//     '0938aa23-f153-4937-9f88-4858b24d6bce',
-//     'e8481c1d-42ea-4610-8e11-1752cfc05a46',
-//   ),
-// );
-
-// console.log(data.species.residents);
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  return species.filter((animal) => ids.some((id) => id === animal.id));
+}
 
 module.exports = getSpeciesByIds;
