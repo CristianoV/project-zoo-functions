bbd546f1706ec3c88742fdde4aae1b092f93445a
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-16 20:58:06 -0300

    requisito dois finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalCorreto = species.find((element) => element.name === animal);
+  const { residents } = animalCorreto;
+  const idade = residents.every((element) => element.age > age);
+  return idade;
 }
 
 module.exports = getAnimalsOlderThan;
