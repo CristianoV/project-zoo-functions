30bac017ce8f81bddda325452c21625547ee7617
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-17 12:28:29 -0300

    terminando o quarto requisito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((nome) => nome.managers.some((ids) => ids === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const nomesObj = employees.filter((empregados) =>
+      empregados.managers.some((nome) => nome === managerId));
+    return nomesObj.map((nome) => `${nome.firstName} ${nome.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -7,4 +7,4 @@ function getSpeciesByIds(...ids) {
 
 module.exports = getSpeciesByIds;
 
-console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
