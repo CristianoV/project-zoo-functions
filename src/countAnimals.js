e6121cf586414264b41908b2a047c5eff34913df
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-19 01:46:05 -0300

    quinto requisito finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function verificacao(acc, element) {
+  acc[element.name] = element.residents.length;
+  return acc;
 }
+
+function countAnimals(animal) {
+  if (!animal) {
+    const normal = species.reduce(verificacao, {});
+    return normal;
+  }
+  if (animal) {
+    const animalEncontrado = species.filter((nome) => nome.name === animal.specie);
+    const quantidadeDeAnimais = animalEncontrado.find((teste) => teste.residents);
+    const { residents } = quantidadeDeAnimais;
+    if (animal.sex) {
+      const animaisPorSexo = residents.filter((arroz) => arroz.sex === animal.sex);
+      return animaisPorSexo.length;
+    }
+    return residents.length;
+  }
+}
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
